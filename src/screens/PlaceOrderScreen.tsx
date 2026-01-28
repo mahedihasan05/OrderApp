9f54261e3b75d9cd0c1f696b6714d17c5e691c40
Md Mahedi Hasan Sarker <mahedihasansarker05@gmail.com>
2026-01-28 21:07:48 +0600

    updated

--- src/screens/PlaceOrderScreen.tsx
+++ src/screens/PlaceOrderScreen.tsx
@@ -1,6 +1,9 @@
-import { View, Text, Button, FlatList } from "react-native";
+import { View, Text, Button, FlatList, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
 import { db, auth } from "../firebase/firebaseConfig";
 import { formatPrice } from "../utils/formatPrice";
 
@@ -11,6 +14,17 @@ const CART_ITEMS = [
 
 export default function PlaceOrderScreen() {
   const navigation = useNavigation<any>();
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
 
   const total = CART_ITEMS.reduce(
     (sum, item) => sum + item.price * item.qty,
@@ -18,10 +32,8 @@ export default function PlaceOrderScreen() {
   );
 
   const confirmOrder = async () => {
-    const user = auth.currentUser;
-
     if (!user) {
-      alert("User not logged in");
+      alert("Please wait, logging in...");
       return;
     }
 
@@ -35,11 +47,13 @@ export default function PlaceOrderScreen() {
     navigation.navigate("OrderHistory");
   };
 
+  if (loading) {
+    return <ActivityIndicator size="large" style={{ marginTop: 50 }} />;
+  }
+
   return (
     <View style={{ padding: 16 }}>
-      <Text style={{ fontSize: 18, fontWeight: "bold", marginBottom: 10 }}>
-        Cart Items
-      </Text>
+      <Text style={{ fontSize: 18, fontWeight: "bold" }}>Cart Items</Text>
 
       <FlatList
         data={CART_ITEMS}
